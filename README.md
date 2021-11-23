55e651b5001562e343186fafa8a8f9b0bdc659cd
René Logala Modia <90214842+logmodia@users.noreply.github.com>
2021-11-23 14:48:13 +0100

    save build

--- README.md
+++
@@ -1,1 +0,0 @@
-## POMODORO-REACT
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import {App} from "./app.jsx";
-
-const app = document.querySelector("#app");
-ReactDOM.render(<App />, app);
